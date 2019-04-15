2f61c8884572c0a745b4e9a03829b481326238aa
Leedan Johnson <leedanjohnson@Gmail.com>
2019-04-15 10:19:40 -0600

    Kinda inital commit

---
+++ AllTodo.Server/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllTodo.Server.Controllers
+{
+    [Route("api/todos")]
+    public class TodoController: Controller
+    {
+        [HttpGet()]
+        public JsonResult GetTodos()
+        {
+            return new JsonResult(new List<Object>()
+            {
+                new {id = 1, Name = "Do this assignment"},
+                new {id = 2, Name = "Celebrate"}
+            });
+        }
+    }
+}
