b46481f3d78ecfef1afb87485e2ac238ea5956f3
Leedan Johnson <leedanjohnson@Gmail.com>
2019-04-16 13:02:35 -0600

    Got all the calls for todo's working (without any authentication)

--- AllTodo.Server/Controllers/TodoController.cs
+++ AllTodo.Server/Controllers/TodoController.cs
@@ -9,7 +9,7 @@ using AllTodo.Shared.Models;
 namespace AllTodo.Server.Controllers
 {
     [Route("api/todos")]
-    public class TodoController: Controller
+    public class TodoController : Controller
     {
         public TodoController(ITodoService todo_service)
         {
@@ -46,6 +46,35 @@ namespace AllTodo.Server.Controllers
             Todo new_todo = this.todo_service.CreateTodo(title, description, (TodoState)state);
 
             return Ok(new_todo);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateTodo(int id, string title, string description, int state = -1)
+        {
+            if (!this.todo_service.Exists(id))
+                return BadRequest();
+
+            Todo existing_todo = this.todo_service.GetTodo(id);
+
+            if (title == null || title == string.Empty)
+                title = existing_todo.Title;
+            if (description == null || description == string.Empty)
+                description = existing_todo.Description;
+            if (state < 0)
+                state = (int)existing_todo.State;
+            if (state < 0 || state > 2)
+                return BadRequest();
+
+            Todo new_todo = this.todo_service.UpdateTodo(id, title, description, (TodoState)state);
+
+            return Ok(new_todo);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodo(int id)
+        {
+            this.todo_service.RemoveTodo(id);
+            return Ok();
         }
     }
 }
